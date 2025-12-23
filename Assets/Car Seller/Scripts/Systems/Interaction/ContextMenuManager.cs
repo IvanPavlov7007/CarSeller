6dd7008ea6ff132494931ada27c77ef12960e98e
IvanPavlov <ivanpavlov7007@gmail.com>
2025-12-23 11:48:35 +0100

    Init contextMenu better implementation, back up

--- Assets/Car Seller/Scripts/Systems/Interaction/ContextMenuManager.cs
+++ Assets/Car Seller/Scripts/Systems/Interaction/ContextMenuManager.cs
@@ -5,8 +5,6 @@ using System.Collections.Generic;
 
 public class ContextMenuManager : Singleton<ContextMenuManager>
 {
-    public Vector2 offsetUp = new Vector2(0, 10);
-    public Vector2 offsetDown = new Vector2(0, -10);
     public GameObject popUpMenuPrefab;
 
     List<PopUpContextMenu> activeMenus = new List<PopUpContextMenu>();
@@ -23,22 +21,67 @@ public class ContextMenuManager : Singleton<ContextMenuManager>
 
     public void CreateContextMenu(GameObject target, UIElement content)
     {
-        GameObject panel = Instantiate(popUpMenuPrefab);
+        GameObject panel = Instantiate(popUpMenuPrefab, ContextMenuCanvas.Instance.RectTransform);
         RectTransform panelTransform = panel.GetComponent<RectTransform>();
 
-        var offset = target.transform.position.y < 0 ? offsetUp : offsetDown;
-        panelTransform.position = target.transform.position + (Vector3)offset;
-        SimpleUIBuilder.Instance.Build(content,panelTransform.GetChild(0).GetChild(0) as RectTransform);
-        createContextMenu(panel, content.blockingInput);
+        SimpleUIBuilder.Instance.Build(content, getContentTransform(panel));
+        createContextMenu(panel, target.transform, content.blockingInput);
+        UpdateContextMenu(panel.GetComponent<PopUpContextMenu>());
     }
 
-    private PopUpContextMenu createContextMenu(GameObject panel, bool blocking)
+    private RectTransform getContentTransform(GameObject panel)
+    {
+        return panel.transform.GetChild(0).GetChild(0) as RectTransform;
+    }   
+
+    private PopUpContextMenu createContextMenu(GameObject panel, Transform target, bool blocking)
     {
         var ctxMenu = panel.AddComponent<PopUpContextMenu>();
+        ctxMenu.Initialize(target);
         activeMenus.Add(ctxMenu);
         if(blocking)
             BlockUIManager.Instance.Block(panel.GetComponent<Canvas>(), ctxMenu.Close);
         return ctxMenu;
+    }
+
+    private void UpdateContextMenu(PopUpContextMenu menu)
+    {
+
+    }
+
+
+    private void Update()
+    {
+        foreach(var menu in activeMenus)
+        {
+            UpdateContextMenu(menu);
+        }
+    }
+
+    public static Vector2 worldScreenHalfSize(Camera cam)
+    {
+        if (cam.orthographic)
+        {
+            float height = cam.orthographicSize;
+            float width = height * cam.aspect;
+
+            return new Vector2(width, height);
+        }
+        else
+        {
+            // For perspective, use field of view and distance
+            float height = Mathf.Tan(cam.fieldOfView * 0.5f * Mathf.Deg2Rad) * -cam.transform.position.z;
+            float width = height * cam.aspect;
+            return new Vector2(width, height);
+        }
+    }
+
+    public static Rect cameraWorldSizeRect(Camera cam)
+    {
+        var halfSize = worldScreenHalfSize(cam);
+        Rect rect = new Rect(Vector2.zero, halfSize * 2f);
+        rect.center = cam.transform.position;
+        return rect;
     }
 
 
--- Assets/Car Seller/Scripts/UI/Systems/ContextMenu/PopUpContextMenu.cs
+++ Assets/Car Seller/Scripts/UI/Systems/ContextMenu/PopUpContextMenu.cs
@@ -2,6 +2,18 @@
 
 public class PopUpContextMenu : MonoBehaviour, IClosable
 {
+    public RectTransform RectTransform { get; private set; }
+    public Transform TargetTransform { get; private set; }
+    private void Awake()
+    {
+        RectTransform = GetComponent<RectTransform>();
+    }
+
+    public void Initialize(Transform targetTransform)
+    {
+        TargetTransform = targetTransform;
+    }
+
     public void Close()
     {
         ContextMenuManager.Instance.closeMenu(this);
